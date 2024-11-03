21b22b3c268dd7377febeac26c7a601dc9c46d16
Wengelawit <wengelawitsamson@gmail.com>
2024-11-03 21:58:40 +0300

    Add UserProfile component to display user information via props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Header />
       <MainContent />
        <Footer />
+       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>

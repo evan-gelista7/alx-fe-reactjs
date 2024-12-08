5136504bd0d47dac1e0b55eb019b4844876fa677
Wengelawit <wengelawitsamson@gmail.com>
2024-12-08 23:45:29 +0300

    Add files to tailwind-react-integration folder

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // This is where Tailwind looks for your content
+  darkMode: "class", // Optional: You can choose 'class' or 'media'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],

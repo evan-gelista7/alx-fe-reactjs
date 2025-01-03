ea0dac49d3f313cd3a02f13865b3b27be6b56008
Wengelawit <wengelawitsamson@gmail.com>
2025-01-04 01:07:40 +0300

    Refactor app to use Context API instead of prop drilling

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,6 +1,17 @@
-// src/UserContext.js
-import React from 'react';
+import React, { createContext, useState } from 'react';
 
-const UserContext = React.createContext(); // Creating context
+// Create the UserContext
+export const UserContext = createContext();
 
-export default UserContext;
+export const UserProvider = ({ children }) => {
+  const [userData, setUserData] = useState({
+    name: "Jane Doe", 
+    email: "jane.doe@example.com"
+  });
+
+  return (
+    <UserContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
